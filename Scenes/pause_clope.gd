f84bf4f27d89378b490dd8efcbe395cea0ecc4cb
EnaFR <enaofficiel@gmail.com>
2025-12-15 18:55:46 +0100

    Animation allumé cigarette
    son de la cigarette mit, le joueur ne peut l'allumer qu'une fois

--- Scenes/pause_clope.gd
+++ Scenes/pause_clope.gd
@@ -1,10 +1,14 @@
 extends Node2D
+@export var Animateur : AnimationPlayer
+var cigarette_allumé = false
+
 func _input(event): 
-	if event.is_action_pressed("allumer"):
-		$Animator.play("Allumer_Clope")
+	if event.is_action_pressed("allumer") and cigarette_allumé == false:
+		Animateur.play("Allumer_Cigarette")
+		cigarette_allumé = true
 
 	if event.is_action_pressed("tirer"):
-		$Animator.play("Tirer_Clope")
+		Animateur.play("Tirer_Cigarette")
 pass
 
 

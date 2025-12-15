24cfcd20ff4626d9ab52fcbe9daec4bbfc807c99
CamilleBlois <blois.camille@hotmail.fr>
2025-12-15 17:08:57 +0100

    Improvment GoOut

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -3,8 +3,35 @@ extends CharacterBody2D
 
 const SPEED = 60.0
 const JUMP_VELOCITY = 0
+var SoundStepPlaying = false
+var SoundOutPlaying = false
+@export var transition : Transitor
+@onready var game_resource : GameResource = load("res://Assets/GameScenes.tres")
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var SoundStep : AudioStream = preload("res://Assets/Step.mp3")
+@onready var SoundDoorOpen : AudioStream = preload("res://Assets/doorOpen.mp3")
+@onready var SoundDoorClose : AudioStream = preload("res://Assets/doorClose.mp3")
+@export var sound : AudioStreamPlayer
 
+func _ready() -> void:
+	# maybe add to the black screen transition
+	sound.stream = SoundDoorOpen
+	sound.play()
+	await sound.finished
+
+func soundWalking():
+	SoundStepPlaying = true
+	sound.pitch_scale = randf_range(0.8,1.2)
+	sound.stream = SoundStep
+	sound.play()
+	await sound.finished
+	SoundStepPlaying = false
+
+func soundOut():
+	SoundOutPlaying = true
+	sound.stream = SoundDoorClose
+	sound.play()
+	await sound.finished
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -21,6 +48,18 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = abs(direction * SPEED)
 		animated_sprite_2d.play()
+		# Walking sound
+		if SoundStepPlaying == false:
+			soundWalking()
+		if position.x >= 500:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			if SoundOutPlaying == false:
+				soundOut()
+				transition.end()
+				await transition.end_done
+				game_resource.game_step += 1
+				get_tree().change_scene_to_packed(game_resource.game_array[game_resource.game_step])
+	
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		animated_sprite_2d.pause()

603eedc6b348d5a08c3e1744e348e48c8245546e
Graou <balthouu@gmail.com>
2025-12-16 13:15:56 +0100

    ciggie scene

--- Scripts/pause_clope.gd
+++ Scripts/pause_clope.gd
@@ -6,8 +6,19 @@ extends Node2D
 @export var scene_id : int
 @onready var game_resource : GameResource = load("res://Assets/GameScenes.tres")
 @export var transition : Transitor
+@export var pixelisation : ColorRect
+@export var smoke : ColorRect
+@export var sprite : AnimatedSprite2D
+
+@onready var shader = pixelisation.material
+@onready var shader_smoke = smoke.material
 
 var cigarette_allumé = false
+var pixelisation_float = 0.001
+var smoke_float = 0.0
+
+func _ready() -> void:
+	shader_smoke.set_shader_parameter("fire_alpha", smoke_float)
 
 func _process(_delta: float) -> void:
 	bar.value = timer.time_left
@@ -16,10 +27,23 @@ func _input(event):
 	if event.is_action_pressed("allumer") and !Animateur.is_playing():
 		if cigarette_allumé:
 			Animateur.play("Tirer_Cigarette")
+			sprite.play("Tire")
+			var tween = get_tree().create_tween()
+			tween.tween_method(func(value): shader.set_shader_parameter("pixelation", value),  pixelisation_float, pixelisation_float+0.007, 1).set_trans(Tween.TRANS_BOUNCE)
+			tween.tween_method(func(value): shader.set_shader_parameter("pixelation", value),  pixelisation_float+0.007, pixelisation_float+0.001, 1)
+			tween.tween_method(func(value): shader_smoke.set_shader_parameter("fire_alpha", value),  smoke_float, smoke_float+0.2, 2).set_trans(Tween.TRANS_BOUNCE)
+			smoke_float+=0.2
+			
+			pixelisation_float += 0.001
 			await Animateur.animation_finished
-			timer.start(timer.time_left-3)
+			if !timer.time_left<=3:
+				timer.start(timer.time_left-3)
 		else:
 			Animateur.play("Allumer_Cigarette")
+			sprite.play("Allume")
+			var tween = get_tree().create_tween()
+			tween.tween_method(func(value): shader_smoke.set_shader_parameter("fire_alpha", value),  smoke_float, smoke_float+0.2, 2).set_trans(Tween.TRANS_BOUNCE)
+			smoke_float+=0.2
 			cigarette_allumé = true
 			timer.start()
 pass
--- character_body_2d.gd
+++ character_body_2d.gd
@@ -39,16 +39,12 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = abs(direction * SPEED)
-		animated_sprite_2d.play()
+		animated_sprite_2d.play("Walking")
 		# Walking sound
 		if SoundStepPlaying == false:
 			soundWalking()
@@ -62,6 +58,6 @@ func _physics_process(delta: float) -> void:
 	
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animated_sprite_2d.pause()
+		animated_sprite_2d.play("Idle")
 
 	move_and_slide()

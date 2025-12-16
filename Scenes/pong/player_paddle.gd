8312c6e816deda1ae47998b525bfe8a2e7282ce5
CamilleBlois <blois.camille@hotmail.fr>
2025-12-16 10:52:19 +0100

    Pong2

--- Scenes/pong/player_paddle.gd
+++ Scenes/pong/player_paddle.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 const SPEED = 200.0
 
 func getWider() -> float:
-	return Input.get_action_strength("down") - Input.get_action_strength("up")
+	return Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 
 func _physics_process(_delta: float) -> void:
 	var direction : Vector2=Vector2(0, getWider())

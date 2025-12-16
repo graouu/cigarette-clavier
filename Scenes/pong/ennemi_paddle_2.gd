743c20cf9f627ed95675c5bdea2c20880efd9772
CamilleBlois <blois.camille@hotmail.fr>
2025-12-16 13:16:10 +0100

    PongAnim

--- Scenes/pong/ennemi_paddle_2.gd
+++ Scenes/pong/ennemi_paddle_2.gd
@@ -1,14 +1,15 @@
 extends CharacterBody2D
 
-var ball : CharacterBody2D
+@export var ball : CharacterBody2D
+
 
 func _ready() -> void:
 	ball = get_parent().get_node("Ball")
 
 func _physics_process(_delta: float) -> void:
-	if ball.global_position.y < 96:
-		global_position.y = 96
-	elif ball.global_position.y > 552:
-		global_position.y = 552
+	if ball.global_position.y < 130:
+		global_position.y = 130
+	elif ball.global_position.y > 384:
+		global_position.y = 384
 	else:
 		global_position.y = ball.global_position.y
